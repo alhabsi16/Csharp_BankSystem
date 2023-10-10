3a20fdf13c6f45d39f5a92220bdf012cb49e0b65
alhabsi16 <mxmx8283@gmail.com>
2023-10-10 11:55:57 +0400

    create account

---
+++ User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_BankSystem
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public List<Account> Accounts { get; set; }
+
+        public User()
+        {
+            Accounts = new List<Account>();
+        }
+        public User(string name, string email, string password)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+            Accounts = new List<Account>();
+        }
+        private string HashPassword(string password)
+        {
+            return password;
+        }
+    }
+}
